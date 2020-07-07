a027dfa786629a1ba925affbc450500206145e0d
unknown <adamshaul360@gmail.com>
2020-07-07 21:24:55 +0300

    fix slick for all tabs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,37 @@
 // Tabs Functionality
-const tabsBtns = document.querySelectorAll('.tabs__btn');
-tabsBtns.forEach( tabBtn => {
-  tabBtn.addEventListener('click', () => {
+function setupTabs() {
 
-    const tabsContent = document.querySelectorAll('.tabs__content');
-    tabsContent.forEach(tabContent => {
-      tabContent.classList.remove('tabs__content--active');
-    });
-
-    const activeSliders = document.querySelectorAll('.slider');
-
-
-    if( tabBtn.id === 'blueTeam' ) {
-      document.querySelector('#blueTeamContent').classList.add('tabs__content--active');
-      const activeateSlider =  document.querySelector('#inBetweenContent .slider');
-        activeateSlider.id = 'currentSlider';
-    }
-    if( tabBtn.id === 'inBetween' ) {
-      document.querySelector('#inBetweenContent').classList.add('tabs__content--active');
-      const activeateSlider =  document.querySelector('#inBetweenContent .slider');
-      activeateSlider.id = 'currentSlider';
-      
-    }
-    if( tabBtn.id === 'redTeam' ) {
-      document.querySelector('#redTeamContent').classList.add('tabs__content--active');
-      const activeateSlider =  document.querySelector('#inBetweenContent .slider');
-      activeateSlider.id = 'currentSlider';
-    }
+  $('.slider').slick({
+    accessibility: true,
+    arrows: false,
+    mobileFirst: true,
+    waitForAnimate: false,
   });
-});
+
+  const tabBtns = document.querySelectorAll('.tabs__btn');
+  tabBtns.forEach(tabBtn => {
+    tabBtn.addEventListener('click', () => {
+      const tab         = tabBtn.dataset.forTeam;
+      const tabContent  = document.querySelectorAll('.tabs__content');
+      const selectedTab = document.querySelector(`.tabs__content[data-team="${tab}"]`);
+
+      tabBtns.forEach(tabBtn => {
+        tabBtn.classList.remove('tabs__btn--active');
+      });
+
+      tabContent.forEach(content => {
+        content.classList.remove('tabs__content--active');
+      });
+
+      tabBtn.classList.add('tabs__btn--active');
+      selectedTab.classList.add('tabs__content--active');
+
+    });
+  });
+}
+
 
 // Slider
 $(document).ready(function() {
-    $('#currentSlider').slick({
-        accessibility: true,
-        arrows: false,
-        mobileFirst: true,
-        waitForAnimate: false,
-      });
+  setupTabs();
 });

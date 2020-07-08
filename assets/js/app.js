bd0b26539eba92e3e5aecc6bba527a78b777c57a
unknown <adamshaul360@gmail.com>
2020-07-08 13:02:34 +0300

    fix slick slider for all tabs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,29 @@
 // Tabs Functionality
 function setupTabs() {
-
-  $('.slider').slick({
-    accessibility: true,
-    arrows: false,
-    mobileFirst: true,
-    waitForAnimate: false,
-  });
-
   const tabBtns = document.querySelectorAll('.tabs__btn');
   tabBtns.forEach(tabBtn => {
     tabBtn.addEventListener('click', () => {
       const tab         = tabBtn.dataset.forTeam;
       const tabContent  = document.querySelectorAll('.tabs__content');
       const selectedTab = document.querySelector(`.tabs__content[data-team="${tab}"]`);
+      const rootStyle = document.documentElement.style;
+
+        
+      $('.slider').slick('unslick');
+      $('.slider').slick('init');
+
+    
+    if( tab === 'blue' ) {
+      rootStyle.setProperty(`--color-teams`, '#01adf9');
+    } else if( tab === 'between' ) {
+      rootStyle.setProperty(`--color-teams`, '#606060');
+    } else if( tab === 'red' ) {
+      rootStyle.setProperty(`--color-teams`, '#FE4164');
+    }
 
       tabBtns.forEach(tabBtn => {
         tabBtn.classList.remove('tabs__btn--active');
+        tabBtn.classList.remove(`tabs__btn--${tab}`);
       });
 
       tabContent.forEach(content => {
@@ -33,5 +40,16 @@ function setupTabs() {
 
 // Slider
 $(document).ready(function() {
+  
+  $('.slider').slick({
+    accessibility: true,
+    arrows: false,
+    mobileFirst: true,
+    waitForAnimate: false,
+    easing: 'ease-in-out'
+  });
+
+
   setupTabs();
+  
 });

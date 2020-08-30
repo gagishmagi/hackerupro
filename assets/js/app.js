3ee8c30ca23169f0b6dcb6e29573968093e44bf0
gagi <gagi.shmagi@gmail.com>
2020-08-30 23:19:03 +0300

    Best senerio yet

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
 
   // $('.slider').slick({
   //   accessibility: true,
-  //   arrows: false,
+  //   arrows: true,
   //   mobileFirst: true,
   //   waitForAnimate: false,
   //   easing: 'ease-in-out'
@@ -95,8 +95,36 @@ expertsSlides.forEach(expertSlide => {
 })
 
 
+function DropDown(el) {
+  this.dd = el;
+  this.placeholder = this.dd.children('span');
+  this.opts = this.dd.find('ul.dropdown > li');
+  this.val = '';
+  this.index = -1;
+  this.initEvents();
+}
+DropDown.prototype = {
+  initEvents: function () {
+    var obj = this;
 
+    obj.dd.on('click', function (event) {
+      $(this).toggleClass('active');
+      return false;
+    });
 
-
+    obj.opts.on('click', function () {
+      var opt = $(this);
+      obj.val = opt.text();
+      obj.index = opt.index();
+      obj.placeholder.text(obj.val);
+    });
+  },
+  getValue: function () {
+    return this.val;
+  },
+  getIndex: function () {
+    return this.index;
+  }
+}
 
 
